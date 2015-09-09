b9ca6d00174dd7df3ae0ba53e6c73511dc8bbd64
Kenny Do <kdo@linkedin.com>
2015-09-08 18:59:00 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='medea',
+    version='0.0.1',
+    description='Media service',
+    long_description='M e d i a s e r v i c e',
+    url='https://github.com/calanimagealpha/medea',
+    author='Cal Animage Alpha',
+    author_email='webmaster@calanimagealpha.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet',
+    ],
+    packages=find_packages(),
+    install_requires=[
+    ],
+    tests_require=[
+    ],
+    entry_points={
+    },
+)
